4d7b2e836cf5d14341badca2c968fab0f676151c
Don Vo <don.vo2@canada.ca>
2021-01-28 16:21:58 -0500

    Add wait class

--- src/main/java/com/d0d00n/multiparttest/controller/SubmissionController.java
+++ src/main/java/com/d0d00n/multiparttest/controller/SubmissionController.java
@@ -1,13 +1,18 @@
 package com.d0d00n.multiparttest.controller;
 
 import java.io.IOException;
+import java.lang.InterruptedException;
+import java.lang.Thread;
+import java.util.Map;
 
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -31,5 +36,29 @@ public class SubmissionController {
                 .contentType(MediaType.parseMediaType("application/octet-stream")).body(resource);
         }
     }
+    
+    @GetMapping("/headerEcho")
+    public String headerEcho(@RequestHeader Map<String, String> headers) {
+    	String headersOutput = "";
+        for (Map.Entry<String, String> tuple : headers.entrySet()) {
+            headersOutput += tuple.getKey() +": "+ tuple.getValue()+'\n';
+        }   	    	
+    	
+    	return headersOutput;
+    }
+	
+    @GetMapping("/wait")
+    public String wait(@RequestHeader Map<String, String> headers) {
+    	Integer waitTime = Integer.parseInt(headers.get("time"));
+		
+		try {
+			Thread.sleep(waitTime * 1000);
+		} catch (InterruptedException ie) {
+			Thread.currentThread().interrupt();
+		}
+    	
+    	return "Me waited " + waitTime + "seconds, boss! Zug zug.";
+    }	
+    
 
 }
